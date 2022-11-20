afd4eec0e013c7b88717b8f010294e7dbd52931e
zilongzhao <zhao.zilo@northeastern.edu>
2022-11-19 19:56:41 -0800

    fixed typo

--- PM4_ZilongZhao/model/Friendships.java
+++ PM4_ZilongZhao/model/Friendships.java
@@ -3,9 +3,9 @@ package blog.model;
 public class Friendships {
 	
 	protected Users user1;
-	protected USers user2;
+	protected Users user2;
 	
-	public Friendships(Users user1, USers user2) {
+	public Friendships(Users user1, Users user2) {
 		super();
 		this.user1 = user1;
 		this.user2 = user2;
@@ -19,11 +19,11 @@ public class Friendships {
 		this.user1 = user1;
 	}
 
-	public USers getUser2() {
+	public Users getUser2() {
 		return user2;
 	}
 
-	public void setUser2(USers user2) {
+	public void setUser2(Users user2) {
 		this.user2 = user2;
 	}
 
